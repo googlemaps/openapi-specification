6266f6c86c975e8175b6d808d2d05b16ae2019ad
Justin Poehnelt <jpoehnelt@google.com>
2021-02-04 00:49:06 +0000

    fix: add header to responses

--- generator/responses/response.ts
+++ generator/responses/response.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,21 @@ import { createReadStream, writeFileSync } from "fs";
 import { exec } from "child_process";
 import path from "path";
 import prettier from "prettier";
+
+const header = `# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+`;
 
 const argv = options({
   archive: {
@@ -99,9 +114,10 @@ const main = async (argv: any) => {
     writeFileSync(
       path.join(argv.output, `${regionTag}.yml`),
       prettier.format(
-        `# [START ${regionTag}_response]
-      ${JSON.stringify(response, null, 2)}
-      # [END ${regionTag}_response]
+        `${header}
+        # [START ${regionTag}_response]
+        ${JSON.stringify(response, null, 2)}
+        # [END ${regionTag}_response]
       `,
         { parser: "yaml" }
       )

29d049dfedd5639ee42485b3949fc87fa45d3426
Justin Poehnelt <jpoehnelt@google.com>
2021-02-24 17:41:17 +0000

    feat: add txt snippets with urls for get requests

--- generator/documentation/build.ts
+++ generator/documentation/build.ts
@@ -15,183 +15,215 @@
  */
 
 import {
-	root,
-	paragraph,
-	text,
-	strong,
-	inlineCode,
-	html as htmlNode,
-	table,
-	tableRow,
-	tableCell,
-	link,
-} from 'mdast-builder';
-import { Parent, Node } from 'unist';
-import { OpenAPIV3 } from 'openapi-types';
-import { isRef } from './helpers';
-import syntax from 'micromark-extension-gfm';
-import gfm from 'mdast-util-gfm';
+  root,
+  paragraph,
+  text,
+  strong,
+  inlineCode,
+  html as htmlNode,
+  table,
+  tableRow,
+  tableCell,
+  link,
+} from "mdast-builder";
+import { Parent, Node } from "unist";
+import { OpenAPIV3 } from "openapi-types";
+import { isRef } from "./helpers";
+import syntax from "micromark-extension-gfm";
+import gfm from "mdast-util-gfm";
 
-import fromMarkdown_ from 'mdast-util-from-markdown';
+import fromMarkdown_ from "mdast-util-from-markdown";
 
 const fromMarkdown = (s: string) =>
-	fromMarkdown_(s, {
-		extensions: [syntax()],
-		mdastExtensions: [gfm.fromMarkdown],
-	});
+  fromMarkdown_(s, {
+    extensions: [syntax()],
+    mdastExtensions: [gfm.fromMarkdown],
+  });
 
 export interface PropertyRow {
-	field: Parent;
-	type: Parent;
-	description?: Parent;
+  field: Parent;
+  type: Parent;
+  description?: Parent;
 }
 
 export const build = (
-	schema: OpenAPIV3.ReferenceObject | OpenAPIV3.ArraySchemaObject | OpenAPIV3.NonArraySchemaObject,
-	key: string
+  schema:
+    | OpenAPIV3.ReferenceObject
+    | OpenAPIV3.ArraySchemaObject
+    | OpenAPIV3.NonArraySchemaObject,
+  key: string
 ): Parent => {
-	if (isRef(schema)) {
-		throw 'cannot handle ref here';
-	}
+  if (isRef(schema)) {
+    throw "cannot handle ref here";
+  }
 
-	console.log(`Generating documentation for ${key}`)
+  console.log(`Generating documentation for ${key}`);
 
-	const title = schema.title == undefined ? key : schema.title;
-	const nodes: any = [];
+  const title = schema.title == undefined ? key : schema.title;
+  const nodes: any = [];
 
-	nodes.push(
-		htmlNode(`<h3 class="schema-object" id="${key}">${title}</h3>`)
-	);
-	if (schema.description) {
-		nodes.push(paragraph(fromMarkdown(schema.description)));
-	}
-	nodes.push(paragraph([text(`type: `), inlineCode(schema.type!)]));
+  nodes.push(htmlNode(`<h3 class="schema-object" id="${key}">${title}</h3>`));
+  if (schema.description) {
+    nodes.push(paragraph(fromMarkdown(schema.description)));
+  }
+  nodes.push(paragraph([text(`type: `), inlineCode(schema.type!)]));
 
-	// if an object, build a table of fields
-	if (schema.type === 'object') {
-		const rows: any = [];
+  // if an object, build a table of fields
+  if (schema.type === "object") {
+    const rows: any = [];
 
-		// header row
-		rows.push(
-			tableRow([
-				tableCell(text('Field')),
-				tableCell(text('Required')),
-				tableCell(text('Type')),
-				tableCell(text('Description')),
-			])
-		);
+    // header row
+    rows.push(
+      tableRow([
+        tableCell(text("Field")),
+        tableCell(text("Required")),
+        tableCell(text("Type")),
+        tableCell(text("Description")),
+      ])
+    );
 
-		// all fields
-		Object.keys(schema.properties!)
-			.sort((a: string, b: string) => {
-				if (schema.required) {
-					if (schema.required.indexOf(a) !== -1 && schema.required.indexOf(b) !== -1) {
-						return a < b ? -1 : 1;
-					} else if (schema.required.indexOf(a) !== -1) {
-						return -1;
-					} else if (schema.required.indexOf(b) !== -1) {
-						return 1;
-					}
-				}
-				return a < b ? -1 : 1;
-			})
-			.forEach((key) => {
-				const required = Boolean(schema.required && schema.required.indexOf(key) !== -1);
+    // all fields
+    Object.keys(schema.properties!)
+      .sort((a: string, b: string) => {
+        if (schema.required) {
+          if (
+            schema.required.indexOf(a) !== -1 &&
+            schema.required.indexOf(b) !== -1
+          ) {
+            return a < b ? -1 : 1;
+          } else if (schema.required.indexOf(a) !== -1) {
+            return -1;
+          } else if (schema.required.indexOf(b) !== -1) {
+            return 1;
+          }
+        }
+        return a < b ? -1 : 1;
+      })
+      .forEach((key) => {
+        const required = Boolean(
+          schema.required && schema.required.indexOf(key) !== -1
+        );
 
-				rows.push(propertyToRow(key, schema.properties![key], required));
-			});
+        rows.push(propertyToRow(key, schema.properties![key], required));
+      });
 
-		nodes.push(table(['left'], rows));
+    nodes.push(table(["left"], rows));
 
-		if (schema.example) {
-			nodes.push(htmlNode(`<h4 class="schema-object-example" id="${key}-example">Example</h4>`));
-			nodes.push(htmlNode(`<pre class="notranslate lang-json prettyprint">${JSON.stringify(schema.example, null, 2)}</pre>`));
-		}
-	}
-	return root(nodes);
+    if (schema.example) {
+      nodes.push(
+        htmlNode(
+          `<h4 class="schema-object-example" id="${key}-example">Example</h4>`
+        )
+      );
+      nodes.push(
+        htmlNode(
+          `<pre class="notranslate lang-json prettyprint">${JSON.stringify(
+            schema.example,
+            null,
+            2
+          )}</pre>`
+        )
+      );
+    }
+  }
+  return root(nodes);
 };
 
 const propertyToRow = (
-	key: string,
-	property: OpenAPIV3.ReferenceObject | OpenAPIV3.ArraySchemaObject | OpenAPIV3.NonArraySchemaObject,
-	required: boolean
+  key: string,
+  property:
+    | OpenAPIV3.ReferenceObject
+    | OpenAPIV3.ArraySchemaObject
+    | OpenAPIV3.NonArraySchemaObject,
+  required: boolean
 ): Parent => {
-	const row: any[] = [];
-	row.push(tableCell(inlineCode(key)));
+  const row: any[] = [];
+  row.push(tableCell(inlineCode(key)));
 
-	// bold if required
-	if (required) {
-		row.push(tableCell([strong(text('required'))]));
-	} else {
-		row.push(tableCell([text('optional')]));
-	}
+  // bold if required
+  if (required) {
+    row.push(tableCell([strong(text("required"))]));
+  } else {
+    row.push(tableCell([text("optional")]));
+  }
 
-	if (isRef(property)) {
-		const name = refName(property);
-		const refLink = link(`#${name}`, name, [text(name)]);
-		row.push(tableCell(refLink));
-		row.push(tableCell(refPropertyDesciption(refLink, property['description'])));
-	} else {
-		if (property.type === 'array') {
-			if (isRef(property.items)) {				
-				const name = refName(property.items);
-				const refLink = link(`#${name}`, name, [text(name)]);				
-				row.push(tableCell([text('Array&lt;'), refLink, text('&gt;')]));
-				row.push(tableCell(refPropertyDesciption(refLink, property.description)));
-			} else if (property.items.type === 'object') {
-				throw `Error: extract object to ref from array items for better documentation.`;
-			} else {
-				const name = property.items.type;
-				row.push(tableCell([text(`Array&lt;${name}&gt;`)]));
-				row.push(tableCell(nonRefPropertyDescription(property, key)));
-			}
-		} else {
-			row.push(tableCell(text(property.type!)));
-			row.push(tableCell(nonRefPropertyDescription(property, key)));
-		}
-	}
+  if (isRef(property)) {
+    const name = refName(property);
+    const refLink = link(`#${name}`, name, [text(name)]);
+    row.push(tableCell(refLink));
+    row.push(
+      tableCell(refPropertyDesciption(refLink, property["description"]))
+    );
+  } else {
+    if (property.type === "array") {
+      if (isRef(property.items)) {
+        const name = refName(property.items);
+        const refLink = link(`#${name}`, name, [text(name)]);
+        row.push(tableCell([text("Array&lt;"), refLink, text("&gt;")]));
+        row.push(
+          tableCell(refPropertyDesciption(refLink, property.description))
+        );
+      } else if (property.items.type === "object") {
+        throw `Error: extract object to ref from array items for better documentation.`;
+      } else {
+        const name = property.items.type;
+        row.push(tableCell([text(`Array&lt;${name}&gt;`)]));
+        row.push(tableCell(nonRefPropertyDescription(property, key)));
+      }
+    } else {
+      row.push(tableCell(text(property.type!)));
+      row.push(tableCell(nonRefPropertyDescription(property, key)));
+    }
+  }
 
-	return tableRow(row);
+  return tableRow(row);
 };
 
 const refPropertyDesciption = (refLink: any, description?: string) => {
-	if (description) {
-		return [paragraph(fromMarkdown(description)), text(' See '), refLink, text(' for more information.')];
-	}
+  if (description) {
+    return [
+      paragraph(fromMarkdown(description)),
+      text(" See "),
+      refLink,
+      text(" for more information."),
+    ];
+  }
 
-	return [text('See '), refLink, text(' for more information.')];
+  return [text("See "), refLink, text(" for more information.")];
 };
 
-const nonRefPropertyDescription = (property: OpenAPIV3.ArraySchemaObject | OpenAPIV3.NonArraySchemaObject, key: string) => {
-	const nodes: Node[] = [];
+const nonRefPropertyDescription = (
+  property: OpenAPIV3.ArraySchemaObject | OpenAPIV3.NonArraySchemaObject,
+  key: string
+) => {
+  const nodes: Node[] = [];
 
-	if (!property.description) {
-		throw `
+  if (!property.description) {
+    throw `
 		
 		
 		Please add a description for '${key}'.
 		
 		
-		`
-	}
+		`;
+  }
 
-	nodes.push(fromMarkdown(property.description));
+  nodes.push(fromMarkdown(property.description));
 
-	if (property.enum) {
-		if (property.description) {
-			nodes.push(text(' '));
-		}
-		nodes.push(paragraph(text('The allowed values include: ')));
-		property.enum.forEach((value, i) => {
-			if (i !== 0) nodes.push(text(', '));
-			if (i == property.enum!.length - 1) nodes.push(text('and '));
-			nodes.push(inlineCode(value));
-
-		});
-	}
-	return nodes;
+  if (property.enum) {
+    if (property.description) {
+      nodes.push(text(" "));
+    }
+    nodes.push(paragraph(text("The allowed values include: ")));
+    property.enum.forEach((value, i) => {
+      if (i !== 0) nodes.push(text(", "));
+      if (i == property.enum!.length - 1) nodes.push(text("and "));
+      nodes.push(inlineCode(value));
+    });
+  }
+  return nodes;
 };
 
-const refName = ({$ref}: OpenAPIV3.ReferenceObject): string => {
-	return $ref.split('/').pop()!.replace("#", "")
-}
+const refName = ({ $ref }: OpenAPIV3.ReferenceObject): string => {
+  return $ref.split("/").pop()!.replace("#", "");
+};
--- generator/documentation/schema.ts
+++ generator/documentation/schema.ts
@@ -14,70 +14,72 @@
  * limitations under the License.
  */
 
-import { createWriteStream, readFileSync } from 'fs';
-import { options } from 'yargs';
-import tar from 'tar-stream';
-import stringify from 'remark-stringify';
-import unified from 'unified';
-import { OpenAPIV3 } from 'openapi-types';
-import gfm from 'remark-gfm';
-import slugify from 'slugify';
-import remarkHtml from 'remark-html';
-import remarkParse from 'remark-parse';
-import prettier from 'prettier';
-import { build } from './build';
+import { createWriteStream, readFileSync } from "fs";
+import { options } from "yargs";
+import tar from "tar-stream";
+import stringify from "remark-stringify";
+import unified from "unified";
+import { OpenAPIV3 } from "openapi-types";
+import gfm from "remark-gfm";
+import slugify from "slugify";
+import remarkHtml from "remark-html";
+import remarkParse from "remark-parse";
+import prettier from "prettier";
+import { build } from "./build";
 
 const argv = options({
-	output: {
-		type: 'string',
-		demandOption: true,
-	},
-	spec: {
-		type: 'string',
-		demandOption: true,
-	},
+  output: {
+    type: "string",
+    demandOption: true,
+  },
+  spec: {
+    type: "string",
+    demandOption: true,
+  },
 }).argv;
 
 const mdProcessor = unified().use(gfm).use(stringify, {
-	bullet: '-',
-	fence: '`',
-	fences: true,
-	incrementListMarker: false,
+  bullet: "-",
+  fence: "`",
+  fences: true,
+  incrementListMarker: false,
 });
 
 const htmlProcessor = unified().use(remarkParse).use(gfm).use(remarkHtml);
 
 const main = async (argv: any) => {
-	const spec = JSON.parse(readFileSync(argv.spec).toString()) as OpenAPIV3.Document;
+  const spec = JSON.parse(
+    readFileSync(argv.spec).toString()
+  ) as OpenAPIV3.Document;
 
-	const pack = tar.pack();
+  const pack = tar.pack();
 
-	for (const key in spec.components!.schemas) {
-		const schema = spec.components!.schemas[key];
-		const regionTag = `maps_http_schema_${slugify(key).toLowerCase()}`;
-		const markdown = mdProcessor.stringify(build(schema, key));
-		// write markdown file
-		pack.entry(
-			{
-				name: `documentation/schemas/${regionTag}.md`,
-			},
-			`<!--- This is a generated file, do not edit! -->\n<!--- [START ${regionTag}] -->\n${markdown}\n<!--- [END ${regionTag}] -->`
-		);
+  for (const key in spec.components!.schemas) {
+    const schema = spec.components!.schemas[key];
+    const regionTag = `maps_http_schema_${slugify(key).toLowerCase()}`;
+    const markdown = mdProcessor.stringify(build(schema, key));
+    // write markdown file
+    pack.entry(
+      {
+        name: `documentation/schemas/${regionTag}.md`,
+      },
+      `<!--- This is a generated file, do not edit! -->\n<!--- [START ${regionTag}] -->\n${markdown}\n<!--- [END ${regionTag}] -->`
+    );
 
-		const html = await htmlProcessor.process(markdown);
-		// write html file
-		pack.entry(
-			{
-				name: `documentation/schemas/${regionTag}.html`,
-			},
-			prettier.format(
-				`<!--- This is a generated file, do not edit! -->\n<!--- [START ${regionTag}] -->\n${html}\n<!--- [END ${regionTag}] -->`,
-				{ parser: 'html' }
-			)
-		);
-	}
-	pack.finalize();
-	pack.pipe(createWriteStream(argv.output));
+    const html = await htmlProcessor.process(markdown);
+    // write html file
+    pack.entry(
+      {
+        name: `documentation/schemas/${regionTag}.html`,
+      },
+      prettier.format(
+        `<!--- This is a generated file, do not edit! -->\n<!--- [START ${regionTag}] -->\n${html}\n<!--- [END ${regionTag}] -->`,
+        { parser: "html" }
+      )
+    );
+  }
+  pack.finalize();
+  pack.pipe(createWriteStream(argv.output));
 };
 
 main(argv);

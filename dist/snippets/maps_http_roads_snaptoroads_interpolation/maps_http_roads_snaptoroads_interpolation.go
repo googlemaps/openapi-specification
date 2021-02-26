88c3b3ab8dfcce296a5dbaac1d6928bb2ce49682
Justin Poehnelt <jpoehnelt@google.com>
2021-02-26 22:49:13 +0000

    wip

--- dist/snippets/maps_http_roads_snaptoroads_interpolation/maps_http_roads_snaptoroads_interpolation.go
+++ dist/snippets/maps_http_roads_snaptoroads_interpolation/maps_http_roads_snaptoroads_interpolation.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-  url := "https://roads.googleapis.com/v1/snapToRoads?path=-35.27801,149.12958%7C-35.28032,149.12907%7C-35.28099,149.12929%7C-35.28144,149.12984%7C-35.28194,149.13003%7C-35.28282,149.12956%7C-35.28302,149.12881%7C-35.28473,149.12836&interpolate=true&key=YOUR_API_KEY"
+  url := "https://roads.googleapis.com/v1/snapToRoads?path=-35.27801,149.12958%7C-35.28032,149.12907%7C-35.28099,149.12929%7C-35.28144,149.12984%7C-35.28194,149.13003%7C-35.28282,149.12956%7C-35.28302,149.12881%7C-35.28473,149.12836&interpolate&key=YOUR_API_KEY"
   method := "GET"
 
   client := &http.Client {

49cf104991cd2bb1e89931b290d43a94ab1e9e2f
Justin Poehnelt <jpoehnelt@google.com>
2021-08-05 15:35:30 +0000

    fix: photoreference to photo_reference

--- dist/snippets/maps_http_places_photo_binary/maps_http_places_photo_binary.java
+++ dist/snippets/maps_http_places_photo_binary/maps_http_places_photo_binary.java
@@ -2,7 +2,7 @@
 OkHttpClient client = new OkHttpClient().newBuilder()
   .build();
 Request request = new Request.Builder()
-  .url("https://maps.googleapis.com/maps/api/place/photo?photoreference=Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT&maxwidth=400&key=YOUR_API_KEY")
+  .url("https://maps.googleapis.com/maps/api/place/photo?photo_reference=Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT&maxwidth=400&key=YOUR_API_KEY")
   .method("GET", null)
   .build();
 Response response = client.newCall(request).execute();
--- dist/snippets/maps_http_places_photo_binary/maps_http_places_photo_binary.rb
+++ dist/snippets/maps_http_places_photo_binary/maps_http_places_photo_binary.rb
@@ -2,7 +2,7 @@
 require "uri"
 require "net/http"
 
-url = URI("https://maps.googleapis.com/maps/api/place/photo?photoreference=Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT&maxwidth=400&key=YOUR_API_KEY")
+url = URI("https://maps.googleapis.com/maps/api/place/photo?photo_reference=Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT&maxwidth=400&key=YOUR_API_KEY")
 
 https = Net::HTTP.new(url.host, url.port)
 https.use_ssl = true

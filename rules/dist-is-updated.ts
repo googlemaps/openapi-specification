e2e900545a9db234fcaf7fb007cc0dea483f3fa8
Justin Poehnelt <jpoehnelt@google.com>
2021-08-16 23:01:55 +0000

    test: exclude postman from diff test due to ids

--- rules/dist-is-updated.ts
+++ rules/dist-is-updated.ts
@@ -14,34 +14,37 @@
  * limitations under the License.
  */
 
-import { options } from 'yargs';
-import tar from 'tar';
-import tmp from 'tmp';
-import { compareSync } from 'dir-compare';
+import { options } from "yargs";
+import tar from "tar";
+import tmp from "tmp";
+import { compareSync } from "dir-compare";
 
 const argv = options({
-	archive: {
-		type: 'string',
-		demandOption: true,
-	},
-	dist: {
-		type: 'string',
-		demandOption: true,
-	},
+  archive: {
+    type: "string",
+    demandOption: true,
+  },
+  dist: {
+    type: "string",
+    demandOption: true,
+  },
 }).argv;
 
 const main = (argv) => {
-	const dir = tmp.dirSync();
+  const dir = tmp.dirSync();
 
-	tar.x({
-		file: argv.archive,
-		sync: true,
-		C: dir.name,
-	});
+  tar.x({
+    file: argv.archive,
+    sync: true,
+    C: dir.name,
+  });
 
-	const res = compareSync('dist', dir.name, { compareContent: true });
-	if (!res.same) {
-		throw `
+  const res = compareSync("dist", dir.name, {
+    compareContent: true,
+    excludeFilter: "**/*-postman.json",
+  });
+  if (!res.same) {
+    throw `
 ${JSON.stringify(res, null, 2)}
 
 
@@ -51,7 +54,7 @@ Please run 'npm run build' to update the dist folder.
 
 
 		`;
-	}
+  }
 };
 
 main(argv);

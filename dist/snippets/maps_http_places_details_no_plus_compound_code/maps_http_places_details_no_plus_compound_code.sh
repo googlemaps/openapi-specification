cd6f7cc130ba185ce93ba21e908fdb6e74ba55ab
Justin Poehnelt <jpoehnelt@google.com>
2021-08-25 09:12:22 -0600

    fix: compound code is optional (#187)

---
+++ dist/snippets/maps_http_places_details_no_plus_compound_code/maps_http_places_details_no_plus_compound_code.sh
@@ -0,0 +1,3 @@
+# [START maps_http_places_details_no_plus_compound_code]
+curl -L -X GET 'https://maps.googleapis.com/maps/api/place/details/json?place_id=GhIJQWDl0CKxOMARUrgehSvwYEA&fields=plus_code&key=YOUR_API_KEY'
+# [END maps_http_places_details_no_plus_compound_code]
